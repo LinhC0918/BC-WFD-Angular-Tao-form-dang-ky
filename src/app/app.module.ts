c2e4c7d2dc997c06c6ebc5471499a10bacd1f9c0
LinhC0918 <nguyenvanlinh12d3@gmail.com>
2018-12-14 11:28:57 +0700

    add new file

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {TodoComponent} from './todo/todo.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RegisterFinalComponent } from './register-final/register-final.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        TodoComponent,
+        LoginComponent,
+        RegisterComponent,
+        RegisterFinalComponent
+    ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
